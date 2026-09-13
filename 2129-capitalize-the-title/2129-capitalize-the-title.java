52054f4f37d1cf56fbd9e5140fbdffb9782d284b
Ankit Saw <167295300+ankitsawdev@users.noreply.github.com>
2026-09-14 00:48:20 +0530

    Time: 3 ms (78.77%), Space: 44 MB (36.27%) - LeetHub

---
+++ 2129-capitalize-the-title/2129-capitalize-the-title.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String capitalizeTitle(String title) {
+
+        String [] words = title.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for(String word : words){
+            if(word.length()<=2){
+                result.append(word.toLowerCase());
+            }else{
+                result.append(Character.toUpperCase(word.charAt(0)));
+                result.append(word.substring(1).toLowerCase());
+            }
+            result.append(" ");
+        }
+
+        return result.toString().trim();
+        
+    }
+}
